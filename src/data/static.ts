0240fe4b55f9c035bd4844ff4e961c337d99e6aa
LalitKushwah <lalitkushwah52525@gmail.com>
2021-04-23 11:14:34 +0530

    update disclaimer

--- src/data/static.ts
+++ src/data/static.ts
@@ -1,6 +1,4 @@
-export const disclaimerMessage = `We are group of people building tools to help get the COVID situation under control out of our free will. We have tried to get data from authenticated sources and verify it as much possible, there was only limited verification we could perform in span of few days. We need people like you to join our effort for better outcome. You do not need to leave your home for joining us. All of us work from our home. Join our team by filling this <a href="https://forms.gle/zutbQqjNxdvmXrre7" target="_blank">form</a>. Connect with us you might have
-Disclaimer:
-Though we have tried to get data from authenticated sources and verify it as much possible, there was only limited verification we could perform in span of few days`
+export const disclaimerMessage = `Though we have tried to get data from authenticated sources and verify it as much possible, there was only limited verification we could perform in span of few days. Please report any gaps in information by writing an email to <b><u>data@coviconnect.in</u></b>. If you can help us find, validate, update details to increase data relevance please join our team by providing some details <a href="https://forms.gle/zutbQqjNxdvmXrre7" target="_blank">here</a> (Even we don't like filling a form but that just increases convenience).`
 
 const data = {
     city: [
